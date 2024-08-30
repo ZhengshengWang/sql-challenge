60b4a6aa3e29b83b8e7e6a324b92606eaee2527f
Zhengsheng Wang <zsheng.wang99@gmail.com>
2024-08-30 01:14:38 -0500

    finished

---
+++ EmployeeSQL/Data Engineering/employee_schema.sql
@@ -0,0 +1,46 @@
+create table titles(
+title_id varchar(50) not null,
+title varchar(50) not null,
+primary key (title_id)
+);
+
+create table departments(
+dept_no varchar(50) not null,
+dept_name varchar(50) not null,
+primary key(dept_no)
+);
+
+create table employees(
+emp_no int not null,
+emp_title_id varchar(50) not null,
+birth_date varchar(50) not null,
+first_name varchar(50) not null,
+last_name varchar(50) not null,
+sex varchar(50) not null,
+hire_date varchar(50) not null,
+primary key (emp_no),
+foreign key (emp_title_id) references titles(title_id)
+);
+
+create table salaries(
+emp_no int not null,
+salary int not null,
+primary key (emp_no),
+foreign key (emp_no) references employees (emp_no)
+);
+
+create table dept_emp(
+emp_no int not null,
+dept_no varchar(50) not null,
+primary key (emp_no,dept_no),
+foreign key (dept_no) references departments(dept_no),
+foreign key (emp_no) references employees (emp_no)
+);
+
+create table dept_manager(
+dept_no varchar(50) not null,
+emp_no int not null,
+primary key (dept_no,emp_no),
+foreign key (dept_no) references departments (dept_no),
+foreign key (emp_no) references employees (emp_no)
+);
